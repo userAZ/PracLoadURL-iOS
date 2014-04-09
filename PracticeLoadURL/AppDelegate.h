d14756039f73bc02718390fdcb0ec6b6838488d3
An Qi <anqi@Macintosh.local>
2014-04-09 18:23:18 -0400

    Initial Commit

---
+++ PracticeLoadURL/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PracticeLoadURL
+//
+//  Created by An Qi on 4/9/14.
+//  Copyright (c) 2014 An Qi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
